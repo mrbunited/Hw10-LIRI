ddb6609e98e212442df5faa44e9bd96b718d58d2
mradke2 <mrbunited@gmail.com>
2018-08-04 07:14:35 -0500

    tweets

---
+++ liri.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var twitter = require("twitter");
+
+var input = process.argv[2];
+var query = process.argv[3];
+console.log(input);
+console.log(query);
+
+function showTweets() { 
+    var client = new twitter(keys.twitterKeys);
+     
+    var params = {screen_name: "newontheblock", count: 20};
+    
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+                if (error) {
+                    console.log("There is an error");
+                }
+                else{
+                    console.log("My 20 latest tweets:")
+                console.log("====================");
+                for (var i=0; i <tweets.length; i++){
+                    console.log("Tweet # " + (i+1) + ".");
+                    console.log(tweets[i]);
+                }
+                }
+
+            })
+
+        };
+
+
+if (input === "my-tweets"){
+showTweets();
+}
+else if (input === "spotify-this-song"){
+
+}
+else if (input === "movie-this"){
+
+}
+else if (input === "do-what-it-says"){
+
+} 
